b9775469a906db3840ac4846eb67d537721e0bbb
bhclaassen <bhclaassen@gmail.com>
2023-09-13 18:15:39 -0700

    FINISHED - R Dataframe to SQL in Long Formate

--- SQL_AddCensusACSData.R
+++ SQL_AddCensusACSData.R
@@ -18,6 +18,39 @@ dbGetQuery(con, 'SELECT * FROM states_info')
 
 # -------------------------------------------------------------------------
 
+setwd("/Users/benclaassen/Documents/_Mines/Data_MAIN/_RawDownloads/Census/ACS 2020 5yr/States/Age and Sex")
+pop1 <- read.csv("ACSST5Y2020.S0101-Data.csv") # Read data
+
+pop2 <- pop1 %>% select(GEO_ID, NAME, S0101_C01_001E, S0101_C01_001M) # Select cols
+
+names(pop2) <- c("GEO_ID", "StateName", "TotalPop_Est", "TotalPop_MOE") # Rename cols
+head(pop2)
+pop2 <- pop2[-1,] # Drop ACS names
+head(pop2)
+
+pop2$Id <- gsub("0400000US", "", pop2$GEO_ID) # Create integer ID col
+
+# Change class of est, moe, and id
+sapply(pop2[,3:5], class)
+pop2[,3:5] <- sapply(pop2[,3:5], as.numeric)
+sapply(pop2[,3:5], class)
+
+# Check [pop2]
+summary(pop2)
+head(pop2)
+
+# Assign all NA's in MOE cols to be equal to 0
+pop3 <- pop2 %>% mutate(TotalPop_MOE = replace_na(TotalPop_MOE, 0))
+summary(pop3)
+head(pop3)
+
+
+pop4 <- pop3 %>% select(Id, StateName, TotalPop_Est, TotalPop_MOE)
+
+head(pop4)
+sapply(pop4, class)
+
+
 pop5 <- pop4[,c(1:2)]
 head(pop5)
 names(pop5) <- c("state_geoid", "state_name")
@@ -28,7 +61,7 @@ pop5$st_var_est <- pop4[,3]
 pop5$st_var_moe <- pop4[,4]
 
 pop5$st_var_source <- "Census ACS"
-pop5$st_var_year <- "2020"
+pop5$st_var_year <- 2020
 pop5$st_var_otherspecs <- "5yr estimates"
 
 head(pop5)
@@ -115,19 +148,14 @@ sql_addCensusDataToDatabase <- function(dbConnection, sqlTableName, dataFrameToA
   )
   
   if( sqlTableColumnNames[1] == "rowid") { # IF first sql table column name is the SERIAL var 'rowid'...
-    if( !( identical( sqlTableColumnNames[-1], names(dataFrameToAdd) ) ) ) { # ...THEN all names except the first must match; ...
+    if( !( identical( sqlTableColumnNames[-1], names(dataFrameToAdd)[1:5] ) ) ) { # ...THEN all names except the first must match; ...
+      print( paste0("Target table names are: {", paste0(sqlTableColumnNames[-1], collapse = ", ") , "} ('rowid' purposely ommited)") ) # Print what the target table names are
       stop("ERROR: Names of input table [dataFrameToAdd] do not match names of [sqlTableName] (Column 1: 'rowid' EXCLUDED)")
     }
   } else if( !( identical( sqlTableColumnNames, names(dataFrameToAdd) ) ) ) { # ...ELSE, first sql table column name is not the SERIAL var 'rowid', THEN all names must match
+    print( paste0("Target table names are: {", paste0(sqlTableColumnNames, collapse = ", ") , "}") ) # Print what the target table names are
     stop("ERROR: Names of input table [dataFrameToAdd] do not match names of [sqlTableName] (Column 1: 'rowid' INCLUDED)")
   }
-  
-  
-# -------------------------------------------------------------------------
-# UPDATED TO HERE ---------------------------------------------------------
-# -------------------------------------------------------------------------
-
-
   
   
   # -----------------------------------------------------------------------
@@ -148,12 +176,7 @@ sql_addCensusDataToDatabase <- function(dbConnection, sqlTableName, dataFrameToA
   for(c in characterVarsList) {
     dataFrameToAdd[,c] <- sapply(dataFrameToAdd[,c], function(x) {paste0("'", x, "'")} )
   }
-
-
-  # Prepare values for SQL formatting -------------------------------------
-  if(variableName == "") {
-    variableName = names(dataFrameToAdd)[2]
-  }
+  
   
   # Create list of values to insert ---------------------------------------
   dataToAdd_values <- "VALUES "
@@ -161,30 +184,20 @@ sql_addCensusDataToDatabase <- function(dbConnection, sqlTableName, dataFrameToA
   for(i in 1:dim(dataFrameToAdd)[1]) {
     dataToAdd_values <- paste0(dataToAdd_values, "(", paste(dataFrameToAdd[i,], collapse= ", "), "), ") # Collapse rows to: "(, , , ),"
   }
-
-# Substitute last comma for a semicolon
-dataToAdd_values <- sub("), $", ");", dataToAdd_values)
-
-
-dataToAdd <- paste0(
-  "INSERT INTO ", sqlTableName, " (state_geoid, state_name, st_var_id, ) ",
-  # "INSERT INTO states_info (state_id, state_name, state_abbrev) ",
-  dataToAdd_values
-)
-dataToAdd
-
-# -------------------------------------------------------------------------
-# -------------------------------------------------------------------------
-
-
-
-
-# -------------------------------------------------------------------------
-# -------------------------------------------------------------------------
-
-
   
+  # Substitute last comma for a semicolon in [dataToAdd_values]
+  dataToAdd_values <- sub("), $", ");", dataToAdd_values)
+  
+  
+  # Assemble final SQL command to insert data
+  dataToAdd_complete <- paste0(
+  "INSERT INTO ", sqlTableName, " (", paste0(names(dataFrameToAdd), collapse = ", "), ") ",
+  # "INSERT INTO ", sqlTableName, " (state_geoid, state_name, st_var_id, ) ",
+  dataToAdd_values
+  )
+  
+  # Final Command
+  # Insert data into [sqlTableName] using command in [dataToAdd_complete]
+  dbGetQuery(, dataToAdd_complete)
 }
-## get sql col names ->
-dbGetQuery(con, 'SELECT * FROM states WHERE false')
 
